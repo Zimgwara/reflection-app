062f812490100f831351662525a775e52a1a4078
Zimgwara <cgimodelling.stl@gmail.com>
2025-09-01 16:57:28 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// This file is intentionally left mostly empty.
-// In the final version of the application, all the necessary JavaScript logic
-// was moved directly into the index.html file to prevent critical loading errors.
-// This file can be deleted from your project in the future if you wish.
-
-console.log("script.js loaded (but is no longer in use).");
-
